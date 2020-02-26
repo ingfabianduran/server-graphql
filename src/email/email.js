75f9c499e5c9baba7253ad8afe4a07d711bc7ed5
feniz18 <ds.martinezv@misena.edu.co>
2020-02-26 07:39:41 -0500

    Se modifica el subject en el correo

--- src/email/email.js
+++ src/email/email.js
@@ -7,7 +7,7 @@ module.exports = {
     sendEmail: async (dataForm, dest) => {
         const dataTransporter = data.getDataEmail(null, null).transporter;
         const dataEmail = data.getDataEmail(dataForm, dest).send;
-        dataEmail.subject = dataForm.dataSoporte.labor;
+        // dataEmail.subject = dataForm.dataSoporte.labor;
         const transporter = nodemailer.createTransport(dataTransporter);
         transporter.use('compile', hbs({
             viewEngine: {
